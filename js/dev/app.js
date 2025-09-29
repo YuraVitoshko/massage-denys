3d21cf5bfcc5d376750caaf1f453b1720f0c4181
Yurii <98182161+YuraVitoshko@users.noreply.github.com>
2025-09-29 21:10:51 +0300

    Add files via upload

---
+++ js/dev/app.js
@@ -0,0 +1,50 @@
+(function polyfill() {
+  const relList = document.createElement("link").relList;
+  if (relList && relList.supports && relList.supports("modulepreload")) return;
+  for (const link of document.querySelectorAll('link[rel="modulepreload"]')) processPreload(link);
+  new MutationObserver((mutations) => {
+    for (const mutation of mutations) {
+      if (mutation.type !== "childList") continue;
+      for (const node of mutation.addedNodes) if (node.tagName === "LINK" && node.rel === "modulepreload") processPreload(node);
+    }
+  }).observe(document, {
+    childList: true,
+    subtree: true
+  });
+  function getFetchOpts(link) {
+    const fetchOpts = {};
+    if (link.integrity) fetchOpts.integrity = link.integrity;
+    if (link.referrerPolicy) fetchOpts.referrerPolicy = link.referrerPolicy;
+    if (link.crossOrigin === "use-credentials") fetchOpts.credentials = "include";
+    else if (link.crossOrigin === "anonymous") fetchOpts.credentials = "omit";
+    else fetchOpts.credentials = "same-origin";
+    return fetchOpts;
+  }
+  function processPreload(link) {
+    if (link.ep) return;
+    link.ep = true;
+    const fetchOpts = getFetchOpts(link);
+    fetch(link.href, fetchOpts);
+  }
+})();
+document.addEventListener("DOMContentLoaded", () => {
+  let time = 30 * 60;
+  const hoursEl = document.querySelector(".timer__hours");
+  const minutesEl = document.querySelector(".timer__minytes");
+  const secondsEl = document.querySelector(".timer__seconds");
+  function updateTimer() {
+    let hours = Math.floor(time / 3600);
+    let minutes = Math.floor(time % 3600 / 60);
+    let seconds = time % 60;
+    hoursEl.innerHTML = `<div class="timer__item">${hours}</div><div class="timer__label">год.</div>`;
+    minutesEl.innerHTML = `<div class="timer__item">${minutes}</div><div class="timer__label">хв.</div>`;
+    secondsEl.innerHTML = `<div class="timer__item">${seconds}</div><div class="timer__label">сек.</div>`;
+    if (time > 0) {
+      time--;
+    } else {
+      clearInterval(timerInterval);
+    }
+  }
+  updateTimer();
+  const timerInterval = setInterval(updateTimer, 1e3);
+});
--- js/dev/index.js
+++ js/dev/index.js
@@ -1,32 +1,4 @@
-(function polyfill() {
-  const relList = document.createElement("link").relList;
-  if (relList && relList.supports && relList.supports("modulepreload")) return;
-  for (const link of document.querySelectorAll('link[rel="modulepreload"]')) processPreload(link);
-  new MutationObserver((mutations) => {
-    for (const mutation of mutations) {
-      if (mutation.type !== "childList") continue;
-      for (const node of mutation.addedNodes) if (node.tagName === "LINK" && node.rel === "modulepreload") processPreload(node);
-    }
-  }).observe(document, {
-    childList: true,
-    subtree: true
-  });
-  function getFetchOpts(link) {
-    const fetchOpts = {};
-    if (link.integrity) fetchOpts.integrity = link.integrity;
-    if (link.referrerPolicy) fetchOpts.referrerPolicy = link.referrerPolicy;
-    if (link.crossOrigin === "use-credentials") fetchOpts.credentials = "include";
-    else if (link.crossOrigin === "anonymous") fetchOpts.credentials = "omit";
-    else fetchOpts.credentials = "same-origin";
-    return fetchOpts;
-  }
-  function processPreload(link) {
-    if (link.ep) return;
-    link.ep = true;
-    const fetchOpts = getFetchOpts(link);
-    fetch(link.href, fetchOpts);
-  }
-})();
+import "./app.min.js";
 let slideUp = (target, duration = 500, showmore = 0) => {
   if (!target.classList.contains("--slide")) {
     target.classList.add("--slide");
@@ -121,18 +93,18 @@ function dataMediaQueries(array, dataSetValue) {
 function spollers() {
   const spollersArray = document.querySelectorAll("[data-fls-spollers]");
   if (spollersArray.length > 0) {
-    let initSpollers2 = function(spollersArray2, matchMedia = false) {
+    let initSpollers = function(spollersArray2, matchMedia = false) {
       spollersArray2.forEach((spollersBlock) => {
         spollersBlock = matchMedia ? spollersBlock.item : spollersBlock;
         if (matchMedia.matches || !matchMedia) {
           spollersBlock.classList.add("--spoller-init");
-          initSpollerBody2(spollersBlock);
+          initSpollerBody(spollersBlock);
         } else {
           spollersBlock.classList.remove("--spoller-init");
-          initSpollerBody2(spollersBlock, false);
+          initSpollerBody(spollersBlock, false);
         }
       });
-    }, initSpollerBody2 = function(spollersBlock, hideSpollerBody = true) {
+    }, initSpollerBody = function(spollersBlock, hideSpollerBody = true) {
       let spollerItems = spollersBlock.querySelectorAll("details");
       if (spollerItems.length) {
         spollerItems.forEach((spollerItem) => {
@@ -154,7 +126,7 @@ function spollers() {
           }
         });
       }
-    }, setSpollerAction2 = function(e) {
+    }, setSpollerAction = function(e) {
       const el = e.target;
       if (el.closest("summary") && el.closest("[data-fls-spollers]")) {
         e.preventDefault();
@@ -167,7 +139,7 @@ function spollers() {
           const spollerSpeed = spollersBlock.dataset.flsSpollersSpeed ? parseInt(spollersBlock.dataset.flsSpollersSpeed) : 500;
           if (!spollersBlock.querySelectorAll(".--slide").length) {
             if (oneSpoller && !spollerBlock.open) {
-              hideSpollersBody2(spollersBlock);
+              hideSpollersBody(spollersBlock);
             }
             !spollerBlock.open ? spollerBlock.open = true : setTimeout(() => {
               spollerBlock.open = false;
@@ -205,7 +177,7 @@ function spollers() {
           });
         }
       }
-    }, hideSpollersBody2 = function(spollersBlock) {
+    }, hideSpollersBody = function(spollersBlock) {
       const spollerActiveBlock = spollersBlock.querySelector("details[open]");
       if (spollerActiveBlock && !spollersBlock.querySelectorAll(".--slide").length) {
         const spollerActiveTitle = spollerActiveBlock.querySelector("summary");
@@ -217,21 +189,20 @@ function spollers() {
         }, spollerSpeed);
       }
     };
-    var initSpollers = initSpollers2, initSpollerBody = initSpollerBody2, setSpollerAction = setSpollerAction2, hideSpollersBody = hideSpollersBody2;
-    document.addEventListener("click", setSpollerAction2);
+    document.addEventListener("click", setSpollerAction);
     const spollersRegular = Array.from(spollersArray).filter(function(item, index, self) {
       return !item.dataset.flsSpollers.split(",")[0];
     });
     if (spollersRegular.length) {
-      initSpollers2(spollersRegular);
+      initSpollers(spollersRegular);
     }
     let mdQueriesArray = dataMediaQueries(spollersArray, "flsSpollers");
     if (mdQueriesArray && mdQueriesArray.length) {
       mdQueriesArray.forEach((mdQueriesItem) => {
         mdQueriesItem.matchMedia.addEventListener("change", function() {
-          initSpollers2(mdQueriesItem.itemsArray, mdQueriesItem.matchMedia);
+          initSpollers(mdQueriesItem.itemsArray, mdQueriesItem.matchMedia);
         });
-        initSpollers2(mdQueriesItem.itemsArray, mdQueriesItem.matchMedia);
+        initSpollers(mdQueriesItem.itemsArray, mdQueriesItem.matchMedia);
       });
     }
   }
@@ -4842,24 +4813,3 @@ class ScrollWatcher {
   }
 }
 document.querySelector("[data-fls-watcher]") ? window.addEventListener("load", () => new ScrollWatcher({})) : null;
-document.addEventListener("DOMContentLoaded", () => {
-  let time = 30 * 60;
-  const hoursEl = document.querySelector(".timer__hours");
-  const minutesEl = document.querySelector(".timer__minytes");
-  const secondsEl = document.querySelector(".timer__seconds");
-  function updateTimer() {
-    let hours = Math.floor(time / 3600);
-    let minutes = Math.floor(time % 3600 / 60);
-    let seconds = time % 60;
-    hoursEl.innerHTML = `<div class="timer__item">${hours}</div><div class="timer__label">год.</div>`;
-    minutesEl.innerHTML = `<div class="timer__item">${minutes}</div><div class="timer__label">хв.</div>`;
-    secondsEl.innerHTML = `<div class="timer__item">${seconds}</div><div class="timer__label">сек.</div>`;
-    if (time > 0) {
-      time--;
-    } else {
-      clearInterval(timerInterval);
-    }
-  }
-  updateTimer();
-  const timerInterval = setInterval(updateTimer, 1e3);
-});
